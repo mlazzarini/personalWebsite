afa77eb44d3f037b9d258a1fc1df10294dff0d66
Margherita Lazzarini <margherita.lazzarini@gmail.com>
2018-07-26 09:23:59 +0200

    Style tables for list sections

--- src/sections/read-it.js
+++ src/sections/read-it.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
+import { Table } from 'react-bootstrap';
+import './read-it.css';
+
 export class ReadIt extends React.Component {
 	render(){
 		return (
 			<div>
-				<h1>Read it</h1>
-				List of the books
+				<p>Here's a list of interesting books/articles/stuff I've read and I recommend!</p>
+				<Table striped>
+				<tbody>
+					<tr>
+						<td>pic 1</td>
+						<td>The pragmatic programmer
+							<br/>
+							Description of the book, blah blahh..
+							<br/>
+							<a href="#"> Link to amazon or boh</a>
+						</td>
+					</tr>
+					<tr>
+					<td>pic 2</td>
+					<td>Jacob</td>
+					</tr>
+					<tr>
+					<td colSpan="2">Larry the Bird</td>
+					</tr>
+				</tbody>
+				</Table>
 			</div>
 		);
 	}
