0f5af7e5ea8cfe3137e5794eafc187dc158c2c9d
Margherita Lazzarini <margherita.lazzarini@gmail.com>
2018-07-31 12:54:25 +0200

    Fulfill and fix read-it section

--- src/sections/read-it.js
+++ src/sections/read-it.js
@@ -1,30 +1,28 @@
 import React from 'react';
 
-import { Table } from 'react-bootstrap';
+import { Table, Image } from 'react-bootstrap';
 import './read-it.css';
 
+import pragmaticProgrammerLogo from '../images/pragmatic_programmer.jpg';
+import buildingMaintainableLogo from '../images/building_maintainable.gif';
+
 export class ReadIt extends React.Component {
-	render(){
+	render() {
 		return (
 			<div>
 				<p>Here's a list of interesting books/articles/stuff I've read and I recommend!</p>
 				<Table striped>
 				<tbody>
 					<tr>
-						<td>pic 1</td>
-						<td>The pragmatic programmer
-							<br/>
-							Description of the book, blah blahh..
-							<br/>
-							<a href="#"> Link to amazon or boh</a>
-						</td>
+						<td><Image src={pragmaticProgrammerLogo} thumbnail /></td>
+						<td>The Pragmatic Programmer: From Journeyman to Master</td>
 					</tr>
 					<tr>
-					<td>pic 2</td>
-					<td>Jacob</td>
+						<td colSpan="2"><a href="https://www.se.rit.edu/~tabeec/RIT_441/Resources_files/How%20To%20Write%20Unmaintainable%20Code.pdf" target="blank" rel="noopener noreferrer">How To Write Unmaintainable Code article</a></td>
 					</tr>
 					<tr>
-					<td colSpan="2">Larry the Bird</td>
+						<td><Image src={buildingMaintainableLogo} thumbnail /></td>
+						<td>Building Maintainable Software book. Here's my <a href="https://medium.com/@mlazzarini/10-maintainable-software-guidelines-94abf872126a" target="blank" rel="noopener noreferrer">book review</a> on Medium.</td>
 					</tr>
 				</tbody>
 				</Table>
