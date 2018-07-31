f54d3c775cd929591b2297f2df521c8d36fe0af0
Margherita Lazzarini <margherita.lazzarini@gmail.com>
2018-07-31 21:04:46 +0200

    Fix target=_blank issue in the contacts section

--- src/sections/contact-me.js
+++ src/sections/contact-me.js
@@ -4,8 +4,8 @@ export class ContactMe extends React.Component {
 	render(){
 		return (
 			<div id="contacts">
-				<span  className="about">Here's my <a href="https://it.linkedin.com/in/margherita-lazzarini-04380a60" target="blank">LinkedIn page</a><br/>
-				Here you can download my <a target="blank" href="./data/onePageCv.pdf"> english CV </a></span><br/>
+				<span  className="about">Here's my <a href="https://it.linkedin.com/in/margherita-lazzarini-04380a60" target="blank" rel="noopener noreferrer">LinkedIn page</a><br/>
+				Here you can download my <a target="blank" rel="noopener noreferrer" href="./data/onePageCv.pdf"> english CV </a></span><br/>
 				<span className="about">E-mail: margherita.lazzarini@gmail.com</span><br/>
 				<span className="about">Skype ID: margheritalazzariniunibo</span>
 				<br/><br/>
