7ed4a5313a0118d45f5f17eff466811c70bb0e3b
Margherita Lazzarini <margherita.lazzarini@gmail.com>
2018-07-31 23:03:52 +0200

    Fix links and styles in the contact section

--- src/sections/contact-me.js
+++ src/sections/contact-me.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import cv from '../data/CV_one_page_Lazzarini.pdf';
+
 export class ContactMe extends React.Component {
 	render(){
 		return (
 			<div id="contacts">
 				<span  className="about">Here's my <a href="https://it.linkedin.com/in/margherita-lazzarini-04380a60" target="blank" rel="noopener noreferrer">LinkedIn page</a><br/>
-				Here you can download my <a target="blank" rel="noopener noreferrer" href="./data/onePageCv.pdf"> english CV </a></span><br/>
-				<span className="about">E-mail: margherita.lazzarini@gmail.com</span><br/>
-				<span className="about">Skype ID: margheritalazzariniunibo</span>
+				Here you can download my <a target="blank" rel="noopener noreferrer" href={cv}> english CV </a></span><br/>
+				<span className="about"><span className="highlighted">E-mail:</span> margherita.lazzarini@gmail.com</span><br/>
+				<span className="about"><span className="highlighted">Skype ID:</span> margheritalazzariniunibo</span>
 				<br/><br/>
 			</div>
 		);
